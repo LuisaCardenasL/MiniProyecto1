8bf4002474b086631dd1c908b6d6f15db5e260d9
Alejandro Tapiero <alejandro.tapiero@correounivalle.edu.co>
2022-05-27 10:37:18 -0500

    Random agregado em código

--- src/miniproyecto1/Codigo.java
+++ src/miniproyecto1/Codigo.java
@@ -1,9 +1,20 @@
 package miniproyecto1;
 
+import java.util.Random;
+
 /**
  *
  * @author invitado
  */
 public class Codigo {
+    private int arCodigo;
     
+    public int getCodigo(){
+        /*
+            Método que genera valor aleatorio a los números de la lista del código.
+        */
+        Random aleatorio = new Random();
+        arCodigo = aleatorio.nextInt(12);
+        return arCodigo;
+    }    
 }
